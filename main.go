ee97a02323e3fad58e82c68283adee7922137768
rykelly <ryank9721@gmail.com>
2026-07-14 17:46:23 +0300

    update main.go to create query log

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"dns-blocker/blocklist"
 	dnsserver "dns-blocker/dns"
+	"dns-blocker/store"
 )
 
 func main() {
@@ -17,7 +18,9 @@ func main() {
 
 	bl.StartAutoReload(24 * time.Hour)
 
+	ql := store.NewQueryLog(1000)
+
 	go dnsserver.StartDoH(":8443", "cert.pem", "key.pem")
 
-	dnsserver.Start(bl)
+	dnsserver.Start(bl, ql)
 }
